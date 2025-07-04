a0bee47fe5ab7280ce1093e1624064eb2eb22181
G. Mag <g.magnenat@gmail.com>
2025-07-04 16:22:59 +0200

    add landing page

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Hello There!
+        </h1>
+        <p class="py-6">
+          This is a travel log app. It's a simple app that allows you to log your travels.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With GitHub
+          <Icon name="tabler:brand-github" size="22" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
