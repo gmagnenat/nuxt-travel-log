b12e25c4fcacde2cd0c0947eee999101149d722f
Gwenaël Magnenat <g.magnenat@gmail.com>
2025-07-11 08:48:52 +0200

    Add auth button and sign out page (#60)

--- components/auth-button.vue
+++ components/auth-button.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 const authStore = useAuthStore();
-const { loading, signIn } = authStore;
 </script>
 
 <template>
-  <button :disabled="loading" class="btn btn-accent" @click="signIn">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown">
+    <div tabindex="0" role="button" class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-6 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="22" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Sign In With GitHub
-    <span v-if="loading" class="loading loading-spinner loading-sm" />
-    <Icon v-else name="tabler:brand-github" size="22" />
+    <Icon name="tabler:brand-github" size="22" />
   </button>
 </template>
