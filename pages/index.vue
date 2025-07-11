ac253e759cd07d7316ac299624762ac9954e9717
G. Mag <g.magnenat@gmail.com>
2025-07-11 09:08:15 +0200

    Redirect to login when accessing protected route

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center">
@@ -8,7 +12,10 @@
         <p class="py-6">
           This is a travel log app. It's a simple app that allows you to log your travels.
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink v-else to="/dashboard" class="btn btn-primary">
+          Start Logging
+        </NuxtLink>
       </div>
     </div>
   </div>
