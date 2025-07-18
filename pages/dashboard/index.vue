1db4ca0af461442294dcdcc1bdbb740a8a829a2a
G. Mag <g.magnenat@gmail.com>
2025-07-18 17:30:09 +0200

    Add sidebar and sidebar-button component

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,3 +1,33 @@
+<script lang="ts" setup>
+import { cn } from "#imports";
+
+const isSidebarOpen = ref(true);
+</script>
+
 <template>
-  <h1>You are logged in</h1>
+  <div class="flex-1 flex">
+    <div
+      :class="cn(
+        'bg-base-100 transition-all duration-300',
+        isSidebarOpen ? 'w-64' : 'w-16',
+      )"
+    >
+      <div
+        :class="cn(
+          'flex hover:cursor-pointer hover:bg-200 p-2',
+          isSidebarOpen ? 'justify-end' : 'justify-center',
+        )"
+        @click="isSidebarOpen = !isSidebarOpen"
+      >
+        <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton :show-label="isSidebarOpen" label="Locations" icon="tabler:map" href="/dashboard" />
+        <SidebarButton :show-label="isSidebarOpen" label="Add Location" icon="tabler:circle-plus-filled" href="/dashboard/add" />
+        <div class="divider" />
+        <SidebarButton :show-label="isSidebarOpen" label="Logout" icon="tabler:logout-2" href="/sign-out" />
+      </div>
+    </div>
+  </div>
 </template>
