492205022910ce56fed37e3ec735faa82e7f1d57
Gwenaël Magnenat <g.magnenat@gmail.com>
2025-07-20 10:27:17 +0200

    Add sidebar (#63)
    * Add clsx and tailwind-merge
    
    * Add sidebar and sidebar-button component
    
    * Save sidebar state in localstorage

---
+++ components/sidebar-button.vue
@@ -0,0 +1,47 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string;
+  icon: string;
+  href: string;
+  showLabel: boolean;
+}>();
+</script>
+
+<template>
+  <div
+    :class="cn(
+      'tooltip-right',
+      !showLabel ? 'tooltip' : undefined,
+    )"
+    :data-tip="showLabel ? undefined : props.label"
+  >
+    <NuxtLink
+      :to="props.href" :class="cn(
+        'flex gap-2 p-2 hover:bg-base-300 hover:cursor-pointer flex-nowrap',
+        !showLabel ? 'justify-center' : 'justify-start',
+      )"
+    >
+      <Icon :name="props.icon" size="24" />
+      <Transition name="grow">
+        <span v-if="showLabel">{{ props.label }}</span>
+      </Transition>
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.grow-enter-active {
+  animation: grow 0.1s;
+}
+.grow-leave-active {
+  animation: grow 0.1s reverse;
+}
+@keyframes grow {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
